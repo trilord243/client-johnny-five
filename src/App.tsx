4279a6e4a4a34f3e0d0471f58f4cbf0e5239ccc9
Sergionx <sergio180203@gmail.com>
2023-11-20 21:34:14 -0400

    Fixed Height

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ function App() {
   return (
     <div className=" w-full h-full min-h-screen">
       <Navbar />
-      <div className="min-h-screen flex items-center justify-center">
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 w-full text-white px-4">
+      <div className="min flex items-center justify-center ">
+        <div
+          className="flex flex-col items-center justify-center bg-gray-900
+          w-full h-[calc(100svh-6.5rem)] text-white px-4"
+        >
           <Form />
         </div>
       </div>
